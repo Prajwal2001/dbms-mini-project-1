b91310e7428b89d899ee4faeb294914d68ee4ad8
Akshay0831 <69149882+Akshay0831@users.noreply.github.com>
2022-01-16 16:21:40 +0530

    SignUp and Login

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, redirect, request, session
+from database import Database
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+db = Database()
+
+@app.route("/")
+def redirectPg():
+    return redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    session["uId"] = None
+    session["email"] = None
+    if request.method == 'POST':
+        if db.checkUser([request.form.get('mailId'), request.form.get('password')]):
+            session["uId"] = db.get_uId_by_email(
+                request.form.get('mailId'))
+            session["email"] = request.form.get('mailId').title()
+            return redirect('/home')
+        else:
+            return render_template("login.html", status=False, title='Login')
+    else:
+        return render_template("login.html", status=True, title='Login')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        response = db.addUser(request.form)
+        return redirect('/login', code=302) if response else render_template("signup.html", status=response)
+    else:
+        return render_template("signup.html", status=True, title='SignUp')
+
+@app.route("/logout")
+def logout():
+    session['uId'] = None
+    session["email"] = None
+    return redirect('/login')
+
+@app.route("/home")
+def home():
+    return render_template("home.html", email=session["email"]) if session['uId'] else redirect('/login', code=302)
+
+if(__name__=='__main__'):
+    app.run(debug=True, port=4000)
---
+++ database.py
@@ -0,0 +1,80 @@
+from hashlib import md5
+import mysql.connector
+
+
+class Database:
+    #Connect Database
+
+    def __init__(self):
+        self.__mydb = mysql.connector.connect(
+            host="localhost",
+            user='root',
+            password='root',
+            database='bookstore',
+            autocommit=True
+        )
+        self.__cursor = self.__mydb.cursor()
+
+    def addUser(self, user: dict):
+        #Adds a user_id to the database, by accepting a list consisting of email and password.
+        #And returns the updated table values in a list of key value pairs
+        try:
+            self.__cursor.execute(
+                f"""INSERT INTO Users VALUES (null, 
+                '{user['email']}', 
+                '{md5(user['password'].encode()).hexdigest()}', 
+                '{user['name']}', 
+                '{user['phone']}', '{str(user['dob'])}')""")
+        except mysql.connector.errors.IntegrityError:
+            return None
+        self.__cursor.execute(f"SELECT * FROM Users")
+
+        return [{
+            "uId": userData[0],
+            "email": userData[1],
+            "password": userData[2],
+            "name": userData[3],
+            "phone": userData[4],
+            "dob": str(userData[5])
+        } for userData in self.__cursor.fetchall()]
+
+    def addAddress(self, address: dict):
+        #Adds a user_id to the database, by accepting a list consisting of email and password.
+        #And returns the updated table values in a list of key value pairs
+        try:
+            self.__cursor.execute(f'''INSERT INTO Users VALUES (
+                    {address['uId']}, 
+                    '{address['address']}', 
+                    '{address['pincode']}')''')
+        except mysql.connector.errors.IntegrityError:
+            return None
+
+        self.__cursor.execute(f"SELECT * FROM Address")
+
+        return [{
+            "uId": userData[0],
+            "address": userData[1],
+            "pincode": userData[2]
+        } for userData in self.__cursor.fetchall()]
+
+    def checkUser(self, userData: list):
+        #Checks if the user_id exists in the database
+        self.__cursor.execute(
+            f"""SELECT *
+                FROM users
+                WHERE email = '{userData[0]}' AND
+                password = '{md5(userData[1].encode()).hexdigest()}'"""
+        )
+        result = self.__cursor.fetchall()
+        return result
+
+    def get_uId_by_email(self, email):
+        #Returns user_id for given user_name"""
+        self.__cursor.execute(f"""
+            SELECT *
+            FROM users
+            WHERE email = '{email}';
+            """)
+        res = self.__cursor.fetchone()
+        return res if res else None
+        
